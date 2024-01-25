923131c459dbd18faaa413706b98998a980d5178
ryder7223 <114200735+ryder7223@users.noreply.github.com>
2024-01-25 08:56:56 +0800

    Update server.py

--- Uploader/server.py
+++ Uploader/server.py
@@ -59,4 +59,4 @@ if __name__ == '__main__':
 
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='192.168.0.175', port=5000, debug=True)
